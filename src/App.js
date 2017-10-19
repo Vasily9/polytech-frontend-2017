70dd16a5ae69302de31b9280a687ed50bf0a9f94
Vasily9 <lvn0509@mail.ru>
2017-10-19 23:06:38 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Nav from './nav.js';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./nav.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-	  
         <header className="App-header">
-		
           <img src={logo} className="App-logo" alt="logo" />
-		  <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-           <Nav />  
+        <Nav />
       </div>
     );
   }
 }
-
-
 
 export default App;
--- src/nav.js
+++ src/nav.js
@@ -1,34 +1,31 @@
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.clickMe = this.clickMe.bind(this);
+    this.state = {
+      isOpened: false
+    };
+  }
 
-class Nav extends Component{
+  clickMe() {
+    this.setState({ isOpened: !this.state.isOpened });
+  }
 
-constructor(props) {
-super(props);
-this.clickMe=this.clickMe.bind(this);
-this.state = {
-	isOpened:false,
-};
-}
+  render() {
+    let dropdownText;
+    if (this.state.isOpened) {
+      dropdownText = <div>Вы нажали на меня</div>;
+    }
 
-clickMe()
-{
-this.setState({isOpened: !this.state.isOpened});
-}
-
-render() {
-	let dropdownText;
-	if (this.state.isOpened)
-	{dropdownText = <div>Вы нажали на меня</div>;}
-	
-return (
-<div onClick={this.clickMe}>
-	Нажмите на меня!
-	{dropdownText}
-</div>
-
-)
-}
+    return (
+      <div onClick={this.clickMe}>
+        Нажмите на меня!
+        {dropdownText}
+      </div>
+    );
+  }
 }
 
 export default Nav;
